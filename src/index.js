d261ebf52ce30554d81119ccf4ae0dd4f5a8595b
mixarik <mixarikhh@gmail.com>
2022-09-04 18:12:06 +0300

    study

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>learn React</li>
+            <li>apply for a job</li>
+            <li>chill</li>
+        </ul>
+    );
+};
+const Header = () => (
+    <div>
+        <h1>First Project List</h1></div>
+
+
+);
+
+const App = () => {
+    return (<div>
+            <Header/>
+            <input placeholder='search'/>
+            <TodoList/>
+        </div>
+    );
+};
+
+
+const reactDom = document.getElementById('root');
+
+ReactDOM.render(<App/>, reactDom);
 
