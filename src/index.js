7665f519a8d978d9347568ac97f8e0c6f40b72fe
ihar <i.arlou@godeltech.com>
2022-09-04 18:27:56 +0300

    zalupa

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>learn React</li>
-            <li>apply for a job</li>
-            <li>chill</li>
-        </ul>
-    );
-};
-const Header = () => (
-    <div>
-        <h1>First Project List</h1></div>
+const TodoList = () => (<ul>
+    <li>learn React</li>
+    <li>apply for a job</li>
+    <li>chill</li>
+</ul>);
 
+const Header = () => (<div>
+    <h1>First Project List</h1>
+</div>);
 
-);
-
-const App = () => {
-    return (<div>
-            <Header/>
-            <input placeholder='search'/>
-            <TodoList/>
-        </div>
-    );
-};
+const App = () => (<div>
+    <Header/>
+    <input placeholder='search'/>
+    <TodoList/>
+</div>);
 
 
 const reactDom = document.getElementById('root');
